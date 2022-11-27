7e143bbd957450c2fdf991979e19263fa29a9a02
BalintKollar852 <balintkollar852@gmail.com>
2022-11-27 11:35:13 +0100

    asd

--- Menu.cs
+++ Menu.cs
@@ -3,25 +3,76 @@ using System;
 
 public class Menu : Node2D
 {
-    Camera2D zoomcamera;
-    private bool zoomin;
-    public override void _Ready()
+	Camera2D zoomcamera;
+	private bool zoomin;
+	private bool zoomout;
+    public bool fullscreen = true;
+    private bool options;
+	private bool fpscounter;
+    Panel optionspanel;
+	public override void _Ready()
+	{
+		zoomcamera = GetNode("Menubackground/Camera2D") as Camera2D;
+        optionspanel = GetNode("OptionsPanel") as Panel;
+	}
+	public override void _Process(float delta)
+	{
+		if(fullscreen){
+			OS.WindowFullscreen = true;
+		}
+		else{
+			OS.WindowFullscreen = false;
+		}
+        if(options && zoomin == false){
+            optionspanel.Visible = true;
+        }
+        else{
+            optionspanel.Visible = false;
+        }
+		if(zoomin){
+			if(zoomcamera.Zoom.x >= 0.4f){ 
+				zoomcamera.Position += new Vector2(0, -11);
+				zoomcamera.Zoom -= new Vector2(0.025f, 0.025f);
+			}
+			else{
+				zoomin = false;
+			}
+		}
+		if(zoomout){
+			if(zoomcamera.Zoom.x <= 0.99f){ 
+				zoomcamera.Position += new Vector2(0, 11);
+				zoomcamera.Zoom += new Vector2(0.025f, 0.025f);
+			}
+			else{
+				zoomout = false;
+			}
+		}
+	}
+	public void _on_Options_pressed(){
+		zoomin = true;   
+        options = true;
+	}
+	public void _on_BackToMenu_pressed(){
+		zoomout = true;
+        options = false;
+	}
+    private void _on_FpsCounter_toggled(bool button_pressed)
     {
-        zoomcamera = GetNode("Menubackground/Camera2D") as Camera2D;
-    }
-    public override void _Process(float delta)
-    {
-        GD.Print(zoomcamera.Zoom);
-        if(zoomin){
-            if(zoomcamera.Zoom.x >= 0.5f){ 
-                zoomcamera.Zoom -= new Vector2(0.1f, 0.1f);
-            }
-            else{
-                zoomin = false;
-            }
+        if(button_pressed){
+            fpscounter = true;
+        }
+        else{
+            fpscounter = false;
         }
     }
-    public void _on_Options_pressed(){
-        zoomin = true;   
+    private void _on_Fullscreen_toggled(bool button_pressed)
+    {
+        if(button_pressed){
+            fullscreen = true;
+        }
+        else{
+            fullscreen = false;
+        }
     }
 }
+
