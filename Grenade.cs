f14a3388580c45a04cdc497008001c9ad9337a7d
Torrente <71377802+BalintKollar852@users.noreply.github.com>
2022-11-29 13:08:02 +0100

    asd

--- Grenade.cs
+++ Grenade.cs
@@ -6,24 +6,27 @@ public class Grenade : Node2D
     [Export] public PackedScene psExplosion;
     private float explosiontime;
     private Vector2 irany;
-    private RigidBody2D grenadebody;
+    private KinematicBody2D grenadebody;
     private Vector2 target;
+    private float speed;
     public override void _Ready()
     {
-        grenadebody = GetNode("RigidBody2D") as RigidBody2D;
+        speed = 50;
+        grenadebody = GetNode("KinematicBody2D") as KinematicBody2D;
         target = GetGlobalMousePosition();
-        irany = Position.DirectionTo(target); 
+        irany = Position.DirectionTo(target) * speed; 
     }
     public override void _Process(float delta)
     {
-        grenadebody.ApplyCentralImpulse(irany);
+        // Error itt
+        grenadebody.MoveAndSlide(irany);
         explosiontime += delta;
         if(explosiontime >= 3){
             QueueFree();
         }
         
     }
-    public void _on_RigidBody2D_tree_exiting(){
+    public void _on_KinematicBody2D_tree_exiting(){
         var gamenode = GetTree().Root.GetNode("Game") as Node2D;
         Node2D explosion = (Node2D)psExplosion.Instance();
         explosion.Position = grenadebody.Position + Position;
